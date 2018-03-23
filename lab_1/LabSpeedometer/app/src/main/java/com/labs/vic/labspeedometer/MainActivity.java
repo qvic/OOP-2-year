064c68fc1f0a25ec8fe49cd0ea62ca8532bd1389
qvic <maliyza1@gmail.com>
2018-03-23 20:50:22 +0200

    added locationmanager

--- lab_1/LabSpeedometer/app/src/main/java/com/labs/vic/labspeedometer/MainActivity.java
+++ lab_1/LabSpeedometer/app/src/main/java/com/labs/vic/labspeedometer/MainActivity.java
@@ -1,15 +1,29 @@
 package com.labs.vic.labspeedometer;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     private TextView mTextMessage;
+    private TextView mTextMessage2;
+
+    private LocationManager mLocationManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,25 +34,86 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_units:
+                    mTextMessage.setText(R.string.title_units);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                case R.id.navigation_gps:
+                    startActivity(new Intent(
+                            android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    return false;
             }
             return false;
         }
     };
 
     @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000, 2, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationManager.removeUpdates(this);
+    }
+
+    private void showLocation(Location location) {
+        if (location == null) {
+            return;
+        }
+
+        mTextMessage.setText(String.format(Locale.getDefault(), "%.4f", location.getLatitude()));
+        mTextMessage2.setText(String.format(Locale.getDefault(), "%.4f", location.getLongitude()));
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        mTextMessage = findViewById(R.id.message);
+        mTextMessage2 = findViewById(R.id.message2);
+        BottomNavigationView navigationView = findViewById(R.id.navigation);
+
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        showLocation(location);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        if (s.equals(LocationManager.GPS_PROVIDER)) {
+            (findViewById(R.id.navigation_gps)).setBackgroundColor(Color.GREEN);
+        } else if (s.equals(LocationManager.NETWORK_PROVIDER)) {
+            (findViewById(R.id.navigation_gps)).setBackgroundColor(Color.YELLOW);
+        }
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
 }
