3e487e62eed6eb1ce1774c79197a7c359f857158
qvic <maliyza1@gmail.com>
2018-03-28 21:21:31 +0300

    added units selector bar, SpeedProvider provides converted data based on selection

--- lab_1/LabSpeedometer/app/src/main/java/com/labs/vic/labspeedometer/GpsSpeedProvider.java
+++ lab_1/LabSpeedometer/app/src/main/java/com/labs/vic/labspeedometer/GpsSpeedProvider.java
@@ -1,19 +1,47 @@
 package com.labs.vic.labspeedometer;
 
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 interface Consumer<T> {
     void accept(T t);
+}
+
+enum SpeedUnit {
+    MS (1, "m/s"), KMH (3.6, "km/h");
+
+    private final double multiplier;
+    private final String name;
+
+    SpeedUnit(double multiplier, String name) {
+        this.multiplier = multiplier;
+        this.name = name;
+    }
+
+    public double getMultiplier() {
+        return multiplier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static double convertMS(SpeedUnit to, double speed) {
+        return speed * to.getMultiplier();
+    }
 }
 
 class GpsSpeedProvider implements LocationListener {
 
     private static final int MIN_TIME = 0;
     private static final int MIN_DISTANCE = 0;
-    private static final double NANOS_TO_SEC = Math.pow(10, -9);
+    private static final double SECONDS_IN_NANO = Math.pow(10, -9);
+
+    private SpeedUnit speedUnit = SpeedUnit.MS;
 
     private Consumer<Double> onSpeedChanged;
     private Consumer<Double> onNativeSpeedChanged;
@@ -23,7 +51,9 @@ class GpsSpeedProvider implements LocationListener {
 
     private Location previousLocation;
 
-    GpsSpeedProvider(LocationManager locationManager,
+    private Context context;
+
+    GpsSpeedProvider(Context context,
                      Consumer<Double> onSpeedChanged,
                      Consumer<Double> onNativeSpeedChanged,
                      Consumer<Location> onLocationChanged) {
@@ -32,7 +62,16 @@ class GpsSpeedProvider implements LocationListener {
         this.onNativeSpeedChanged = onNativeSpeedChanged;
         this.onLocationChanged = onLocationChanged;
 
-        this.locationManager = locationManager;
+        this.context = context;
+        this.locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    void setSpeedUnit(SpeedUnit speedUnit) {
+        this.speedUnit = speedUnit;
+    }
+
+    SpeedUnit getSpeedUnit() {
+        return speedUnit;
     }
 
     @Override
@@ -41,22 +80,25 @@ class GpsSpeedProvider implements LocationListener {
             return;
         }
 
+        Log.i("Location", String.valueOf(location.getExtras().getInt("satellites")));
+
         onLocationChanged.accept(location);
 
+        // weighted average or smth
         if (previousLocation == null) {
             previousLocation = location;
             return;
         }
 
         double distance = previousLocation.distanceTo(location);
-        double timeElapsed = NANOS_TO_SEC * (location.getElapsedRealtimeNanos() -
+        double timeElapsed = SECONDS_IN_NANO * (location.getElapsedRealtimeNanos() -
                 previousLocation.getElapsedRealtimeNanos());
 
         double speed = distance / timeElapsed;
-        double speed2 = location.getSpeed();
+        double speedNative = location.getSpeed();
 
-        onSpeedChanged.accept(speed);
-        onNativeSpeedChanged.accept(speed2);
+        onSpeedChanged.accept(SpeedUnit.convertMS(speedUnit, speed));
+        onNativeSpeedChanged.accept(SpeedUnit.convertMS(speedUnit, speedNative));
     }
 
     @Override
--- lab_1/LabSpeedometer/app/src/main/java/com/labs/vic/labspeedometer/MainActivity.java
+++ lab_1/LabSpeedometer/app/src/main/java/com/labs/vic/labspeedometer/MainActivity.java
@@ -1,18 +1,16 @@
 package com.labs.vic.labspeedometer;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,7 +24,25 @@ public class MainActivity extends AppCompatActivity {
     private TextView speedView1;
     private TextView speedView2;
 
+    private BottomNavigationView unitsMenuView;
+    private boolean unitsMenuHidden = true;
+
     private GpsSpeedProvider speedProvider;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onUnitsSelected = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.units_ms:
+                    speedProvider.setSpeedUnit(SpeedUnit.MS);
+                    return true;
+                case R.id.units_kmh:
+                    speedProvider.setSpeedUnit(SpeedUnit.KMH);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -35,10 +51,17 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    unitsMenuView.animate().translationY(0);
+                    unitsMenuHidden = true;
                     return true;
                 case R.id.navigation_units:
+                    unitsMenuView.setVisibility(View.VISIBLE);
+                    unitsMenuView.animate().translationY(-unitsMenuView.getHeight());
+                    unitsMenuHidden = false;
                     return true;
                 case R.id.navigation_gps:
+                    unitsMenuView.animate().translationY(0);
+                    unitsMenuHidden = true;
                     startActivity(new Intent(
                             android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
                     return false;
@@ -47,44 +70,72 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private BottomNavigationView.OnNavigationItemReselectedListener mOnNavigationItemReselectedListener
+            = new BottomNavigationView.OnNavigationItemReselectedListener() {
+        @Override
+        public void onNavigationItemReselected(@NonNull MenuItem item) {
+            if (item.getItemId() == R.id.navigation_units) {
+                if (unitsMenuHidden) {
+                    unitsMenuView.animate().translationY(-unitsMenuView.getHeight());
+                    unitsMenuHidden = false;
+                } else {
+                    unitsMenuView.animate().translationY(0);
+                    unitsMenuHidden = true;
+                }
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        latitudeView = findViewById(R.id.latitudeView);
-        longitudeView = findViewById(R.id.longitudeView);
-        accuracyView = findViewById(R.id.accuracyView);
+        latitudeView = findViewById(R.id.latitude_view);
+        longitudeView = findViewById(R.id.longitude_view);
+        accuracyView = findViewById(R.id.accuracy_view);
         speedView1 = findViewById(R.id.display1);
         speedView2 = findViewById(R.id.display2);
+
+        unitsMenuView = findViewById(R.id.units_menu);
+        unitsMenuView.setOnNavigationItemSelectedListener(onUnitsSelected);
+
         BottomNavigationView navigationView = findViewById(R.id.navigation);
 
         navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigationView.setOnNavigationItemReselectedListener(mOnNavigationItemReselectedListener);
 
-        speedProvider = new GpsSpeedProvider((LocationManager) this.getSystemService(Context.LOCATION_SERVICE),
+        speedProvider = new GpsSpeedProvider(this,
                 new Consumer<Double>() {
                     @Override
                     public void accept(Double speed) {
-                        speedView1.setText(String.format(Locale.getDefault(), "%.3f m/s", speed));
+                        speedView1.setText(
+                                String.format(Locale.getDefault(), "%.3f %s",
+                                        speed, speedProvider.getSpeedUnit().getName()));
                     }
-                }, new Consumer<Double>() {
-            @Override
-            public void accept(Double speed) {
-                speedView2.setText(String.format(Locale.getDefault(), "%.3f m/s", speed));
-            }
-        }, new Consumer<Location>() {
-            @Override
-            public void accept(Location location) {
-                latitudeView.setText(String.format(Locale.getDefault(), "%s: %f",
-                        getString(R.string.title_latitude), location.getLatitude()));
+                },
+                new Consumer<Double>() {
+                    @Override
+                    public void accept(Double speed) {
+                        speedView2.setText(
+                                String.format(Locale.getDefault(), "%.3f %s",
+                                        speed, speedProvider.getSpeedUnit().getName()));
+                    }
+                },
+                new Consumer<Location>() {
+                    @Override
+                    public void accept(Location location) {
+                        latitudeView.setText(String.format(Locale.getDefault(), "%s: %f",
+                                getString(R.string.title_latitude), location.getLatitude()));
 
-                longitudeView.setText(String.format(Locale.getDefault(), "%s: %f",
-                        getString(R.string.title_longitude), location.getLongitude()));
+                        longitudeView.setText(String.format(Locale.getDefault(), "%s: %f",
+                                getString(R.string.title_longitude), location.getLatitude()));
 
-                accuracyView.setText(String.format(Locale.getDefault(), "%s: %.1f",
-                        getString(R.string.title_accuracy), location.getAccuracy()));
-            }
-        });
+                        accuracyView.setText(String.format(Locale.getDefault(), "%d satellites give %.1f m %s",
+                                location.getExtras().getInt("satellites"),
+                                location.getAccuracy(), getString(R.string.title_accuracy).toLowerCase()));
+                    }
+                });
     }
 
     @Override
@@ -98,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             speedProvider.resume();
-        } catch (Exception ignored) {}
+        } catch (SecurityException ignored) {}
     }
 
     @Override
