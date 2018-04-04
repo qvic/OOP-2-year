8bc2359de0be677973f86d6cb010671e00e30ad9
qvic <maliyza1@gmail.com>
2018-04-04 22:50:49 +0300

    trying to test gpsspeedprovider with changing locations

--- lab_1/LabSpeedometer/app/src/test/java/com/labs/vic/labspeedometer/GpsSpeedProviderTest.java
+++ lab_1/LabSpeedometer/app/src/test/java/com/labs/vic/labspeedometer/GpsSpeedProviderTest.java
@@ -8,57 +8,71 @@ import com.labs.vic.labspeedometer.helpers.Consumer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GpsSpeedProviderTest {
+    private static final double DELTA = 0.0000001;
+
     @Mock
-    LocationManager locationManager;
+    private LocationManager locationManager;
 
     @Test
     public void locationChangeTest() {
-        final Location mockedLocation = mock(Location.class);
-//        final ArgumentCaptor<Double> latitudeCapture = ArgumentCaptor.forClass(Double.class);
-//        final ArgumentCaptor<Double> longitudeCapture = ArgumentCaptor.forClass(Double.class);
-
-//        when(mockedLocation.getLatitude()).thenCallRealMethod();
-//        doCallRealMethod().when(mockedLocation).setLatitude(any(Double.class));
-
-//        doNothing().when(mockedLocation).setLatitude(latitudeCapture.capture());
-//        doNothing().when(mockedLocation).setLongitude(longitudeCapture.capture());
-
         final GpsSpeedProvider gpsSpeedProvider = new GpsSpeedProvider.Builder(locationManager)
-        .setOnLocationChanged(new Consumer<Location>() {
-            @Override
-            public void accept(Location location) {
-                assertEquals(location.getLatitude(), 12.0, 0.0001);
-                assertEquals(location.getLongitude(), 11.0, 0.0001);
-            }
-        })
-        .build();
+                .setOnLocationChanged(new Consumer<Location>() {
+                    @Override
+                    public void accept(Location location) {
+                        assertNotNull(location);
+
+                        assertNotEquals(location.getLatitude(), 0.0, DELTA);
+                        assertNotEquals(location.getLongitude(), 0.0, DELTA);
+
+                        assertEquals(location.getLatitude(), 50.0, DELTA);
+                        assertEquals(location.getLongitude(), 30.0, DELTA);
+                    }
+                })
+                .setOnSpeedChanged(new Consumer<Double>() {
+                    @Override
+                    public void accept(Double speed) {
+                        assertEquals(speed, 13.22, DELTA);
+                    }
+                })
+                .build();
 
         doAnswer(new Answer() {
             @Override
-            public Void answer(InvocationOnMock invocation) throws Throwable {
-                Location location = mock(Location.class);
-                doReturn(12.0).when(location).getLatitude();
-                doReturn(11.0).when(location).getLongitude();
-                gpsSpeedProvider.onLocationChanged(location);
+            public Void answer(InvocationOnMock invocation) {
+                Location loc1 = mock(Location.class);
+
+                doReturn(50.0).when(loc1).getLatitude();
+                doReturn(30.0).when(loc1).getLongitude();
+                doReturn(1.0f).when(loc1).getAccuracy();
+                doReturn(1000000L).when(loc1).getElapsedRealtimeNanos();
+
+                gpsSpeedProvider.onLocationChanged(loc1);
+
+                Location loc2 = mock(Location.class);
+
+                doReturn(50.00001).when(loc2).getLatitude();
+                doReturn(30.00001).when(loc2).getLongitude();
+                doReturn(1.0f).when(loc2).getAccuracy();
+                doReturn(2000000L).when(loc2).getElapsedRealtimeNanos();
+
+                gpsSpeedProvider.onLocationChanged(loc2);
+                // TODO: fix testing speed, gpsSpeedProvider won't change 'cause it's final
+
                 return null;
             }
         }).when(locationManager).requestLocationUpdates(any(String.class),
