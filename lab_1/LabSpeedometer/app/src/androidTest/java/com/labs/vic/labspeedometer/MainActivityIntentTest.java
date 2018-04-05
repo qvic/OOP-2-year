3f8f46d7f874f1579ef8a6209426ba59333557a0
qvic <maliyza1@gmail.com>
2018-04-05 22:26:23 +0300

    added espresso ui test for MainActivity

---
+++ lab_1/LabSpeedometer/app/src/androidTest/java/com/labs/vic/labspeedometer/MainActivityIntentTest.java
@@ -0,0 +1,29 @@
+package com.labs.vic.labspeedometer;
+
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityIntentTest {
+
+    @Rule
+    public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule<>(MainActivity.class);
+
+    @Test
+    public void testNavigationView_GPS() {
+        onView(withId(R.id.navigation_gps)).perform(click());
+
+        intended(toPackage("com.android.settings"));
+    }
+}
--- lab_1/LabSpeedometer/app/src/androidTest/java/com/labs/vic/labspeedometer/MainActivityTest.java
+++ lab_1/LabSpeedometer/app/src/androidTest/java/com/labs/vic/labspeedometer/MainActivityTest.java
@@ -1,13 +1,46 @@
 package com.labs.vic.labspeedometer;
 
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.PositionAssertions.isBottomAlignedWith;
+import static android.support.test.espresso.assertion.PositionAssertions.isCompletelyAbove;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testNavigationView_Units() {
+        onView(withId(R.id.navigation_units)).perform(click());
+        onView(withId(R.id.units_menu)).check(isCompletelyAbove(withId(R.id.navigation_units)));
+        onView(withId(R.id.units_kmh)).perform(click());
+        onView(withId(R.id.units_menu)).check(isBottomAlignedWith(withId(R.id.container)));
+
+        onView(withId(R.id.display_large)).check(matches(withText("0.00 km/h")));
+        onView(withId(R.id.display_small)).check(matches(withText("0.00 km/h")));
+
+        onView(withId(R.id.navigation_units)).perform(click());
+        onView(withId(R.id.units_menu)).check(isCompletelyAbove(withId(R.id.navigation_units)));
+        onView(withId(R.id.units_ms)).perform(click());
+
+        onView(withId(R.id.display_large)).check(matches(withText("0.00 m/s")));
+        onView(withId(R.id.display_small)).check(matches(withText("0.00 m/s")));
+
+        onView(withId(R.id.navigation_units)).perform(click());
+        onView(withId(R.id.navigation_home)).perform(click());
+        onView(withId(R.id.units_menu)).check(isBottomAlignedWith(withId(R.id.container)));
+    }
 }
