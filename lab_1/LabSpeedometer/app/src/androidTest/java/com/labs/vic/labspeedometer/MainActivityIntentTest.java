2e324a8c20e00daaa7d00523c1ee1b5a2855ec86
qvic <maliyza1@gmail.com>
2018-04-05 22:29:01 +0300

    fixed freezing on settings activity while running tests

--- lab_1/LabSpeedometer/app/src/androidTest/java/com/labs/vic/labspeedometer/MainActivityIntentTest.java
+++ lab_1/LabSpeedometer/app/src/androidTest/java/com/labs/vic/labspeedometer/MainActivityIntentTest.java
@@ -1,8 +1,11 @@
 package com.labs.vic.labspeedometer;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,8 +13,11 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.IsNot.not;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -19,6 +25,13 @@ public class MainActivityIntentTest {
 
     @Rule
     public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule<>(MainActivity.class);
+
+    @Before
+    public void stubAllExternalIntents() {
+        intending(not(isInternal()))
+                .respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
+    }
+
 
     @Test
     public void testNavigationView_GPS() {
