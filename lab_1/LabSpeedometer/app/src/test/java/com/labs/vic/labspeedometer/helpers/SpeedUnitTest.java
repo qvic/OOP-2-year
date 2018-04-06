27c093faa7ede5275795a0b4d4ab5a3d82ef2a7d
qvic <maliyza1@gmail.com>
2018-04-06 10:04:13 +0300

    modified SpeedUnitTest, .gitignore

--- lab_1/LabSpeedometer/app/src/test/java/com/labs/vic/labspeedometer/helpers/SpeedUnitTest.java
+++ lab_1/LabSpeedometer/app/src/test/java/com/labs/vic/labspeedometer/helpers/SpeedUnitTest.java
@@ -9,10 +9,15 @@ public class SpeedUnitTest {
     private static final double DELTA = 0.000001;
 
     @Test
-    public void convertMS() {
+    public void convertMStoKMH() {
         assertEquals(SpeedUnit.convertMS(SpeedUnit.KMH, 123.35), 444.06, DELTA);
         assertEquals(SpeedUnit.convertMS(SpeedUnit.KMH, 10.0), 36.0, DELTA);
         assertEquals(SpeedUnit.convertMS(SpeedUnit.KMH, 0.0), 0.0, DELTA);
+        assertEquals(SpeedUnit.convertMS(SpeedUnit.MS, 20.0), 20.0, DELTA);
+    }
+
+    @Test
+    public void convertMStoMS() {
         assertEquals(SpeedUnit.convertMS(SpeedUnit.MS, 20.0), 20.0, DELTA);
     }
 }
