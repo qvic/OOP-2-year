d0ec965f5d41a2283529e9b9fe17755e4b09af57
qvic <maliyza1@gmail.com>
2018-05-04 09:50:25 +0300

    added loading files,\nreplaced send button with load button

--- lab_3/LabSockets/src/main/java/ui/MainController.java
+++ lab_3/LabSockets/src/main/java/ui/MainController.java
@@ -6,14 +6,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
 import models.Message;
 import models.Messages;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.util.Date;
 import java.util.ResourceBundle;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainController implements Initializable {
 
@@ -24,20 +24,50 @@ public class MainController implements Initializable {
     private TextArea editorArea;
 
     @FXML
-    private Button sendButton;
+    private Button loadButton;
 
-    private void onSendAction(ActionEvent actionEvent) {
-        Message request = new Message(Messages.Type.TEXT, editorArea.getText(), socketClient.getAddress(), new Date());
-        socketClient.send(request);
+    private void onLoadAction(ActionEvent actionEvent) {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Open");
+//        chooser.setInitialDirectory(
+//                new File(System.getProperty("/home/vic"))
+//        );
+//        chooser.getExtensionFilters().add(
+//                new FileChooser.ExtensionFilter("Text files", "*.txt")
+//        );
+        File file = chooser.showOpenDialog(loadButton.getScene().getWindow());
+        if (file == null) return;
+
+        try (FileReader fileReader = new FileReader(file);
+             BufferedReader reader = new BufferedReader(fileReader)) {
+
+            StringBuilder builder = new StringBuilder();
+            String line = reader.readLine();
+
+            while (line != null) {
+                builder.append(line);
+                builder.append(System.lineSeparator());
+                line = reader.readLine();
+            }
+
+            editorArea.setText(builder.toString());
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        loadButton.setOnAction(this::onLoadAction);
+
         try {
             socketClient = new SocketClient("localhost", 8080,
                     message -> {
                         ignoreUpdate = true;
                         editorArea.setText(message.getBody());
+                        editorArea.positionCaret(message.getBody().length());
                         ignoreUpdate = false;
                     });
 
