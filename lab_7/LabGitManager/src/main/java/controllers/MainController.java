29217cbcc2b4819d84049c8361981e029998b072
qvic <maliyza1@gmail.com>
2018-06-09 00:40:55 +0300

    basic functionality, commit count statistics

--- lab_7/LabGitManager/src/main/java/controllers/MainController.java
+++ lab_7/LabGitManager/src/main/java/controllers/MainController.java
@@ -4,28 +4,39 @@ import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import models.GitWrapper;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Config;
-import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+    public static final String DEFAULT_GROUP = "Default";
     @FXML
     TreeTableView<GitWrapper> groupsTable;
 
     @FXML
     Button addRepoButton;
 
+    @FXML
+    Button addGroupButton;
+
+    @FXML
+    Button showStatisticsButton;
+
     private TreeItem<GitWrapper> root;
 
-    private void onLoadAction(ActionEvent actionEvent) {
+    private void onAddRepoAction(ActionEvent actionEvent) {
         DirectoryChooser chooser = new DirectoryChooser();
         chooser.setTitle("Open git repository");
         chooser.setInitialDirectory(
@@ -33,22 +44,95 @@ public class MainController implements Initializable {
         );
         File selectedDirectory = chooser.showDialog(addRepoButton.getScene().getWindow());
         if (selectedDirectory == null) return;
-        try {
-            try (Git git = Git.open(selectedDirectory)) {
-                TreeItem<GitWrapper> treeItem = new TreeItem<>(new GitWrapper(git, git.getRepository().getDirectory().toString()));
-                root.getChildren().add(treeItem);
+
+        try (Git git = Git.open(selectedDirectory)) {
+            TreeItem<GitWrapper> newItem = new TreeItem<>(
+                    new GitWrapper(git, git.getRepository().getDirectory().getParentFile().getName()));
+
+            TreeItem<GitWrapper> selectedGroup = groupsTable.getSelectionModel().getSelectedItem();
+            if (selectedGroup != null) {
+                selectedGroup.setExpanded(true);
+                selectedGroup.getChildren().add(newItem);
+            } else {
+                root.setExpanded(true);
+                root.getChildren().get(0).getChildren().add(newItem);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Exception");
+            alert.setHeaderText("Not a repository!");
+            alert.setContentText(e.getMessage());
+            alert.showAndWait();
         }
 
     }
 
+    private void onAddGroupAction(ActionEvent actionEvent) {
+        TextInputDialog dialog = new TextInputDialog("New group");
+        dialog.setTitle("Add group");
+        dialog.setHeaderText("Creating new group");
+        dialog.setContentText("Enter new group name:");
+
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(name -> {
+            TreeItem<GitWrapper> item = new TreeItem<>(new GitWrapper(null, name));
+            root.getChildren().add(item);
+        });
+    }
+
+    private void showStatistics(ActionEvent actionEvent) {
+        Dialog dialog = new Dialog();
+        dialog.setTitle("Statistics");
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+
+        TreeItem<GitWrapper> group = groupsTable.getSelectionModel().getSelectedItem();
+        if (group == null) {
+            group = root.getChildren().get(0);
+        }
+
+        VBox content = new VBox();
+        PieChart pieChart = new PieChart();
+        content.getChildren().add(new Label("Commits statistics for " + group.getValue().getName()));
+        content.getChildren().add(pieChart);
+
+        for (TreeItem<GitWrapper> gitWrapperTreeItem : group.getChildren()) {
+
+            Git git = gitWrapperTreeItem.getValue().getGit();
+            int count = 0;
+            Iterable<RevCommit> commits;
+
+            try {
+                commits = git.log().call();
+            } catch (GitAPIException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Exception");
+                alert.setHeaderText("Git API Exception");
+                alert.setContentText(e.getMessage());
+                alert.showAndWait();
+                return;
+            }
+            for (RevCommit revCommit : commits) {
+                count++;
+            }
+            pieChart.getData().add(new PieChart.Data(gitWrapperTreeItem.getValue().getName(), count));
+        }
+
+
+        dialog.getDialogPane().setContent(content);
+        dialog.showAndWait();
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        root = new TreeItem<>(new GitWrapper(null, "Group 1"));
+        root = new TreeItem<>();
         root.setExpanded(true);
+        groupsTable.setShowRoot(false);
         groupsTable.setRoot(root);
+
+        TreeItem<GitWrapper> defaultGroup = new TreeItem<>(new GitWrapper(null, DEFAULT_GROUP));
+        defaultGroup.setExpanded(true);
+        root.getChildren().add(defaultGroup);
 
         TreeTableColumn<GitWrapper, String> pathColumn = new TreeTableColumn<>("Path");
         pathColumn.setPrefWidth(200);
@@ -70,7 +154,9 @@ public class MainController implements Initializable {
         groupsTable.getColumns().add(pathColumn);
         groupsTable.getColumns().add(nameColumn);
 
-        addRepoButton.setOnAction(this::onLoadAction);
+        showStatisticsButton.setOnAction(this::showStatistics);
+        addRepoButton.setOnAction(this::onAddRepoAction);
+        addGroupButton.setOnAction(this::onAddGroupAction);
     }
 }
 
